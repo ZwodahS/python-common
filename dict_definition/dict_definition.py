28296fb42fad0cc06a74811dd8faa9e4f9f7a61c
Eric Ng <ericnjf@gmail.com>
2015-03-11 12:28:52 +0800

    (new) add DatetimeField and cleaner mixin

--- dict_definition/dict_definition.py
+++ dict_definition/dict_definition.py
@@ -1,4 +1,23 @@
-
+#           DO WHAT THE F*** YOU WANT TO PUBLIC LICENSE
+#                   Version 2, December 2004
+#
+# Copyright (C) 2015- ZwodahS(github.com/ZwodahS)
+# zwodahs.github.io
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#           DO WHAT THE F*** YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE F*** YOU WANT TO.
+#
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
 import re
 
 """
@@ -207,6 +226,22 @@ class ListField(TypedField):
             else:
                 for inner in value:
                     yield from self.inner_type.errors(inner, None)
+
+
+class DateTimeField(Field):
+    """Field used to store datetime object.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def errors(self, value, with_key=None):
+        yield from super().errors(value, with_key)
+        if not isinstance(value, (datetime.datetime, )):
+            if with_key is not None:
+                yield (with_key, Field.ERROR_TYPE, value)
+            else:
+                yield Field.ERROR_TYPE
 
 
 class DictField(TypedField):
